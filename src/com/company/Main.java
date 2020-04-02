a57b1b31f0ec580cbc7af4b594f8b52542435e01
google <ogrttt@gmail.com>
2020-04-02 03:04:20 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// В процесее сделаю до 3тьего, вчера отключили электричество, успел ночью только сделать вторую, на 3тьею сил не хватило
+    }
+}
